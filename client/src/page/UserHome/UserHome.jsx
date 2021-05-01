9a0e526194eec09fd68565947d2ecc5225d834b7
robertop96 <roberto.manzano96@gmail.com>
2021-05-01 03:14:04 -0500

    Complete user home page and breakpoints
    [Ticket: X]

--- client/src/page/UserHome/UserHome.jsx
+++ client/src/page/UserHome/UserHome.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
-import './UserHome.sass';
+import './UserHome.scss';
+import Card from '../../components/Card/Card';
+import session from '../../assets/Images/Session-background.jpeg';
+import { Link } from 'react-router-dom';
 
 function UserHome() {
+  const GameSession = [
+    {
+      image: session,
+      title: "Orlando's Session",
+      description: "Orlando's First Adventurer's Game"
+    },
+    {
+      image: session,
+      title: 'Chris Session',
+      description: 'Chris Crazy Adventure'
+    },
+    {
+      image: session,
+      title: 'Lenin Last Chance',
+      description: 'Will he survive!?'
+    }
+  ];
+
   return (
-    <div>
-      <h1>User Home</h1>
-    </div>
+    <section className="welcome-section">
+      <article className="welcome-wrapper">
+        <article className="welcome">
+          <h1 className="welcome__title">Welcome Roberto</h1>
+        </article>
+        <div className="welcome__buttons">
+          <Link to="/game/id" className="welcome__buttons--button">
+            Create Game
+          </Link>
+          <button className="welcome__buttons--button">Join Game</button>
+        </div>
+      </article>
+      <article className="sessions-wrapper">
+        <Link className="sessions-wrapper__cards" to="/game/id">
+          <Card information={GameSession} />
+        </Link>
+      </article>
+    </section>
   );
 }
 
