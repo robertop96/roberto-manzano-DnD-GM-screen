a2572c51997f17a30c0728982c3da94a90f4b186
robertop96 <roberto.manzano96@gmail.com>
2021-05-05 07:47:33 -0500

    Heroku trial
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]

--- client/src/components/Button/Button.jsx
+++ client/src/components/Button/Button.jsx
@@ -1,14 +1,14 @@
 import './Button.scss';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Button({ text }) {
   return (
     <section className="call-to-action">
       <div className="call-to-action--button">
-        <Link className="call-to-action--link" to="/sign/up">
+        <NavLink className="call-to-action--link" to="/sign/up">
           {text}
-        </Link>
+        </NavLink>
       </div>
     </section>
   );
